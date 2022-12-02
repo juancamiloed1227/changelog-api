f60e957e185c93039f0855a445a680a1f5a7780d
Juan Camilo Espinosa <juancamilo12.jced@gmail.com>
2022-12-02 15:45:58 -0500

    Ready to build

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use(cors());
 app.use("/api", routes);
 
 // Start the server on port 3000
-app.listen(3000, () => {
-    console.log("API server listening on port 3000");
+app.listen(process.env.PORT || 8080, () => {
+    console.log("API server listening");
 });
